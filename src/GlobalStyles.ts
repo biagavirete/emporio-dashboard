14448ea84f5511bc3fc6587887ec2b4fb9c67e25
biagavirete <biagavirete@gmail.com>
2021-03-11 23:36:33 -0300

    feat: create global styles and add assets

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300;700&display=swap');
+
+:root {
+  --yellow: #ffa935;
+  --bright-yellow: #FDFE01;
+  --dark: #2b2e34;
+  --dark-grey: #8e8e8e;
+  --light-grey: #f0f0f0;
+  --orange: #fc591f;
+  --green: rgb(53, 200, 92);
+  --black: #000;
+  --background: #F2ECFC;
+}
+
+  * {
+    margin: 0;
+    border: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Lato', sans-serif;
+  }
+
+  body {
+    width: 100vw; 
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #2b2e34;
+    background-color: #D8CAFD;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Helvetica', sans-serif;
+    font-weight: 700;
+  }
+`;
+
+export default GlobalStyle;
