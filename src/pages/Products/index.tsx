44c00c31603b52be62fdefb295f4bd9843b6a908
biagavirete <biagavirete@gmail.com>
2021-03-12 21:01:24 -0300

    feat: create products page

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,10 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { IoTrashOutline } from 'react-icons/io5';
 import { useDispatch, useSelector } from 'react-redux';
+import NewProductForm from '../../components/NewProductForm';
+import Sidebar from '../../components/Sidebar';
+import UserInfo from '../../components/UserInfo';
 import * as ProductsService from '../../store/ducks/products/actions';
 import { Product } from '../../store/ducks/products/types';
+import { MainContainer } from '../Dashboard/styles';
 
 const Products = () => {
+  const [showAddNewProduct, setShowAddNewProduct] = useState(false);
+
   const dispatch = useDispatch();
+
+  const { role } = useSelector((state: any) => state.users.data)
+
+  const toggleAddNewProduct = () => {
+    setShowAddNewProduct(!showAddNewProduct);
+  }
 
   useEffect(() => {
     dispatch(ProductsService.getProductsRequest());
@@ -21,21 +34,58 @@ const Products = () => {
     }
     dispatch(ProductsService.getProductsRequest())
   }
+
+  useEffect(() => {
+    dispatch(ProductsService.getProductsRequest());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [products])
+
   return (
-    <>
-      <h1>Produtos disponíveis</h1>
-      <table>
-        {products !== undefined && products.map((row: Product) => (
-          <>
-            <tr>
-              <td>{row.title}</td>
-              <td>{row.price}</td>
-              <td><button onClick={() => deleteProduct(row.id)}>Excluir</button></td>
-            </tr>
-          </>
-        ))}
-      </table>
-    </>
+    <MainContainer>
+      <Sidebar />
+      <div className="content-area">
+        <h1>Produtos disponíveis</h1>
+        <div className="table">
+          {role === 'admin' ? (
+            <>
+              <table>
+                <tbody>
+                  {products !== undefined && products.map((row: Product) => (
+                    <>
+                      <tr>
+                        <td>{row.title}</td>
+                        <td>{row.price}</td>
+                        <td><button onClick={() => deleteProduct(row.id)}><IoTrashOutline size={20} /></button></td>
+                      </tr>
+                    </>
+                  ))}
+                </tbody>
+              </table>
+            </>
+          ) : (
+            <>
+              <table>
+                {products !== undefined && products.map((row: Product) => (
+                  <>
+                    <tr>
+                      <td>{row.title}</td>
+                      <td>{row.price}</td>
+                    </tr>
+                  </>
+                ))}
+              </table>
+            </>
+          )}
+        </div>
+        <div className="new-product">
+          <button onClick={toggleAddNewProduct}>Adicionar novo produto</button>
+
+          {showAddNewProduct && <NewProductForm />}
+
+        </div>
+      </div>
+      <UserInfo />
+    </MainContainer>
   );
 }
 
---
+++ src/pages/Products/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const NewProductForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+
+  input {
+      margin-top: 0.5rem;
+      height: 3rem;
+      width: 20rem;
+      padding: 0 1rem;
+      border: 1px solid var(--light-grey);
+      border-radius: 0.8rem;
+    }
+
+    button {
+      height: 3.1rem;
+      width: 15rem;
+      margin-top: 1rem;
+      background: var(--bright-yellow);
+      border-radius: 0.8rem;
+      color: #000000;
+      font-size: 1.2rem;
+      font-weight: bold;
+      transition: all 0.2s;
+
+      :hover {
+        cursor: pointer;
+        background: #FEFF65;
+        box-shadow: 0px 5px 5px rgba(75, 93, 104, 0.1);
+      }
+    }
+
+    p {
+      font-size: 0.7rem;
+      color: var(--orange);
+      font-weight: bold;
+      padding: 0.3rem;
+    }
+`;
