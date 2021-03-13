e9aeb7cba588a1f05be41e7bfe9415148c3689fb
biagavirete <biagavirete@gmail.com>
2021-03-13 17:46:27 -0300

    feat: style dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,26 +1,19 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import * as UsersService from '../../store/ducks/users/actions';
 import * as ProductsService from '../../store/ducks/products/actions';
 import Sidebar from '../../components/Sidebar';
 import UserInfo from '../../components/UserInfo';
-import { MainContainer } from './styles';
+import { MainContainer, SquareDiv } from './styles';
+import EmporioLogo from '../../assets/svgs/LogoEmporio';
 
 const Dashboard = () => {
 
-  const [adminAuth, setAdminAuth] = useState(false);
-  const { name, role } = useSelector((state: any) => state.users.data)
+  const { name } = useSelector((state: any) => state.users.data)
   const token = localStorage.getItem("token")
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (role === 'admin') {
-      setAdminAuth(true);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
 
   useEffect(() => {
     dispatch(UsersService.loadUsersRequest());
@@ -43,15 +36,20 @@ const Dashboard = () => {
           <MainContainer>
             <Sidebar />
             <div className="content-area">
-              <h1>Dashboard</h1>
-              <p>Bem-vindo(a), {name}</p>
-              {adminAuth ? <p>Pode ver tudo</p> : <p>Não vê users</p>}
-              <Link to="/users">Clique para ver os usuários</Link>
-              <div className="user-quantity">
-                <strong>{usersList.length}</strong>
-              </div>
-              <div className="products-quantity">
-                <strong>{products.length}</strong>
+              <h1>Bem-vindo(a), {name}!</h1>
+              <p>Confira abaixo os dados referentes ao site:</p>
+              <div className="data-area">
+                <EmporioLogo />
+                <div className="data">
+                  <SquareDiv>
+                    <p>{usersList.length}</p>
+                    <strong>usuários cadastrados</strong>
+                  </SquareDiv>
+                  <SquareDiv>
+                    <p>{products.length}</p>
+                    <strong>produtos cadastrados</strong>
+                  </SquareDiv>
+                </div>
               </div>
             </div>
             <UserInfo />
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,9 +3,8 @@ import styled from 'styled-components';
 export const MainContainer = styled.div`
   width: calc(100vw - 30rem);
   height: calc(100vh - 10rem);
-  background: var(--background);
+  background: #ffffff;
   border-radius: 2rem;
-  padding: 3rem 2rem;
   box-shadow: 0px 5px 5px rgba(75, 93, 104, 0.1);
   display: grid;
   grid-template-columns: 25% 50% 25%;
@@ -13,6 +12,85 @@ export const MainContainer = styled.div`
   "sidenav content userinfo";
 
   .content-area {
+    display: flex;
+    flex-direction: column;
+    padding: 3rem 0 3rem 0;
     overflow: auto;
+
+    h1 {
+      margin-bottom: 2rem;
+    }
+
+    .data-area {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      justify-items: center;
+      align-content: center;
+      align-items: center;
+      margin-top: 2rem;
+
+      .data {
+        margin-top: 2rem;
+        display: flex;
+        flex-direction: row; 
+
+        > div {
+          margin-right: 2rem;
+        }
+      }
+    }
+  }
+
+  @media(max-width: 1280px) {
+    .content-area {
+
+    .data-area {
+
+      .data {
+        margin-top: 2rem;
+        display: flex;
+        flex-direction: column; 
+
+        > div {
+          margin-bottom: 2rem;
+        }
+      }
+    }
+  }
+  }
+`;
+
+export const SquareDiv = styled.div`
+  width: 10rem;
+  height: 10rem;
+  border-radius: 1rem;
+  box-shadow: 0px 5px 5px rgba(75, 93, 104, 0.1);
+  background-color: var(--light-grey);
+  padding: 1rem 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+
+  p {
+    font-family: 'Open Sans Condensed', sans-serif;
+    font-weight: 700;
+    font-size: 3rem;
+    margin-bottom: 0.5rem;
+    color: var(--yellow);
+  }
+
+  @media(max-width: 1028px) {
+    width: 6rem;
+    height: 6rem;
+
+    p {
+      font-size: 2rem;
+    }
+    
+    strong {
+      font-size: 0.8rem;
+    }
   }
 `;
